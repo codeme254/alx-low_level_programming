3aa69a5486d041990577a72b7218257c77d58cab
codeme254 <otwomadennis365@gmail.com>
2022-07-01 15:13:56 +0300

    correction 1

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  *main - Entry point
@@ -7,7 +8,8 @@
 
 int main(void)
 {
-	for (int x = 'A'; x <= 'Z'; x++)
+	int x;
+	for (x = 'A'; x <= 'Z'; x++)
 	{
 		putchar(tolower(x));
 	}
