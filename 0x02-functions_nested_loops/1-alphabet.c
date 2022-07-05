1772ff72c208c457556dc896491ff463bb4a3ef5
codeme254 <otwomadennis365@gmail.com>
2022-07-05 12:36:58 +0300

    question 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  *main - check the code
+  *print_alphabet - some good ole code
+  *Return: always 0
+**/
+
+void print_alphabet()
+{
+	int n;
+	for(n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
