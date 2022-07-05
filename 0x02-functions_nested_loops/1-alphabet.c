839b0d08547754a5107404029c055f02c2b5af1a
codeme254 <otwomadennis365@gmail.com>
2022-07-05 13:02:54 +0300

    enforcing betty coding style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,11 @@
 
 /**
   *main - check the code
-  *print_alphabet - some good ole code
+  *print_alphabet - function that prints alphabet lettes in lower case
   *Return: always 0
 **/
 
-void print_alphabet()
+void print_alphabet(void)
 {
 	int n;
 	for(n = 'a'; n <= 'z'; n++)
