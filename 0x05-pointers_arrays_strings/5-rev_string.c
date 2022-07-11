94aadb74c6f51f9e526575b67eeb92781066670f
codeme254 <otwomadennis365@gmail.com>
2022-07-11 13:35:05 +0300

    question 6

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+  *rev_string - function that reverses a string
+  *@s : input string
+**/
+
+void rev_string(char *s)
+{
+	int i, len, temp;
+
+	len = strlen(s);
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+	puts((const char *)s);
+}
+
