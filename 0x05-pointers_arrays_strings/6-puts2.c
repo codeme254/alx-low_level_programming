db6ab8feaa55ad9dd92357475f70b313a20f174e
codeme254 <otwomadennis365@gmail.com>
2022-07-11 14:57:18 +0300

    question 7 correction hard final

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <string.h>
 #include <stdio.h>
-/** 
+/**
   *puts2 - function that prints every other character of a string
   *@str : input string
 **/
@@ -10,7 +10,7 @@ void puts2(char *str)
 {
 	int i, len;
 
-	len = strlen(str);	
+	len = strlen(str);
 	/*for (i = 0; str[i] != '\n' && str[i] != '\0' ; i+=2)*/
 	for (i = 0; i < len; i += 2)
 	{
