afb4596fdc1b9573598ffec52b5b26f70b6f2bfe
codeme254 <otwomadennis365@gmail.com>
2022-07-20 14:16:40 +0300

    question 2  correction

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,14 +8,5 @@
 
 int _strlen_recursion(char *s)
 {
-	int count;
-
-	count = 0;
-
-	if (*s != '\0')
-	{
-		count++;
-		_strlen_recursion(*s - 1);
-	}
-	return (count);
+	return *s ? 1 + _strlen_recursion(s + 1) : 0;
 }
