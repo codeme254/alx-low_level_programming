0f372b92f1b08450601a58c80a9de01b25e28fe3
codeme254 <otwomadennis365@gmail.com>
2022-07-20 15:44:20 +0300

    question 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,9 +8,11 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y == 1)
-	{
+	if (y < 0)
+		return -1;
+	else if (y == 0)
+		return 1;
+	else if (y == 1)
 		return (x);
-	}
 	return (base * _pow_recursion(x, y - 1));
 }
