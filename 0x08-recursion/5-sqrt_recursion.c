0c284fdc24bfbbc0033dd791c82af876608eff77
codeme254 <otwomadennis365@gmail.com>
2022-07-20 17:27:55 +0300

    today rest, it was a tough day

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  *_sqrt_recursion - calculates the natural square root of a given integer
+  *@n : input integer
+  *Return: natural square root
+**/
+
+int _sqrt_recursion(int n)
+{
+	/*code here*/
+}
