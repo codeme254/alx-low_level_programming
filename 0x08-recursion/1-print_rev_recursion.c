78606df65795b5124bc279bfc203c8f90f28d169
codeme254 <otwomadennis365@gmail.com>
2022-07-20 13:50:13 +0300

    question 2 correction

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,12 +6,12 @@
   *@s : input string
 **/
 
-int _print_rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 		_print_rev_recursion(s+1);
 	else
-		return
+		putchar(*s);
 	putchar(*s);
 }
 
