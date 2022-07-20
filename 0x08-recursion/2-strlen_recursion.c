77adce26cc25e00ff0ca5a685c9ff94dee7e380a
codeme254 <otwomadennis365@gmail.com>
2022-07-20 14:20:27 +0300

    betty coding style correction

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,9 +4,10 @@
 /**
   *_strlen_recursion - recursively finds the length of a string
   * @s : input string
+  *return : the length of the string input
 **/
 
 int _strlen_recursion(char *s)
 {
-	return *s ? 1 + _strlen_recursion(s + 1) : 0;
+	return (*s ? 1 + _strlen_recursion(s + 1) : 0;)
 }
