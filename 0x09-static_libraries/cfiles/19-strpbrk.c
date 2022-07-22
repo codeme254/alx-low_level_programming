0a136660b3ca1a0ebcf3d3bfd0f25e4287c46c92
codeme254 <otwomadennis365@gmail.com>
2022-07-22 12:41:36 +0300

    questin 1

---
+++ 0x09-static_libraries/cfiles/19-strpbrk.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  *_strpbrk - function of this program
+  *@s : first param
+  *@accept : second param
+**/
+
+char *_strpbrk(char *s, char *accept)
+{
+	/*code here*/
+}
+
---
+++ 0x09-static_libraries/cfiles/5-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  *_isupper - checks if a character is in uppercase
+  *@c : input character or alphabet
+  *Return: 1 if the character is in alphabet
+**/
+
+int _isupper(int c)
+{
+	/*check whether the character is alphabet or not*/
+}
+
