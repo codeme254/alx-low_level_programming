4533afe73ee1f9e620497d884189cd3d76e8a8f9
codeme254 <otwomadennis365@gmail.com>
2022-07-23 12:41:25 +0300

    question 0 correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+  *main - function that prints it's name and the main function of the program
+  *@argc : number of arguements passed during terminal execution
+  *@argv : list of arguements passed
+  *Return: Always 0 (Success)
+  **/
+
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
