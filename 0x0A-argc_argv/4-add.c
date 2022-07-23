16758d1a1a9271932a798032deda2458c244a423
codeme254 <otwomadennis365@gmail.com>
2022-07-23 13:44:03 +0300

    task 4 correction 5

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,31 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "holberton.h"
 /**
-  *main - function that adds positive numbers
-  *@argc : number of arguements
-  *@argv : arguements passed ie the digits in this case
-  *Return: 0 success
-  */
-
+ * main - Entry Point
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
-	int i, result = 0;
+	int i, sum = 0;
 
 	if (argc < 1)
-	{
 		return (0);
-	}
+
 	for (i = 1; i < argc; i++)
 	{
 		if (!atoi(argv[i]))
 		{
-			printf("Error\n");
+			printf("%s\n", "Error");
 			return (1);
 		}
-		result += atoi(argv[i]);
+		sum += atoi(argv[i]);
 	}
-	printf("%d\n", result);
+	printf("%d\n", sum);
+
 	return (0);
 }
-
