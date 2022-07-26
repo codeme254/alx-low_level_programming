8696c6f613a65703b93efbe645c7d63f9ca23c62
codeme254 <otwomadennis365@gmail.com>
2022-07-26 11:43:49 +0300

    task 1 corr 1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	array = malloc(sizeof(*array) * size);
 
 	if (size == 0 || array == NULL)
-		return (null);
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		array[i] = c;
