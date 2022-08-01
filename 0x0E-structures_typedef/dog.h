5549199cde0cd8984a9e0aa7bd6e20424fe0dfb6
codeme254 <otwomadennis365@gmail.com>
2022-08-01 09:40:37 +0300

    task 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  * struct dog - structure describing a dog
+  * @name: name of the dog
+  * @age: age of the dog
+  * @owner: owner of the dog
+  * Description: structure describing a dog
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*DOG_H*/
