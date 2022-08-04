2ee994ebf4fa369722e98247c8dc1aa91b70a8a4
codeme254 <otwomadennis365@gmail.com>
2022-08-04 14:49:02 +0300

    another correction for 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array == NULL || cmp == NULL)
 		return (-1);
 
-	if (size <= 0 || !array || !cmp)
+	if (size <= 0)
 		return (-1);
 
 	for (i = 0; i < size - 1; i++)
