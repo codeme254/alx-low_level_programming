b86b5f5be1996570aaad75b92a69f8b76d772248
codeme254 <otwomadennis365@gmail.com>
2022-08-04 14:50:22 +0300

    task 2 correction 4

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,10 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL)
-		return (-1);
-
-	if (size <= 0)
+	if (size <= 0 || !array || !cmp)
 		return (-1);
 
 	for (i = 0; i < size - 1; i++)
