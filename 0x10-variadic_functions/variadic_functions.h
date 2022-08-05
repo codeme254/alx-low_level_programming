033ba1145b492c6ef796beeeec54c1e06317c1ea
codeme254 <otwomadennis365@gmail.com>
2022-08-05 12:50:37 +0300

    task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,4 +3,5 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *seperator, const unsigned int n, ...);
 void print_strings(const char *seperator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif /*VARIADIC_FUNCTIONS_H*/
