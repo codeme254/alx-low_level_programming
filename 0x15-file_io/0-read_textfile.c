168c624a187bfbbc63079b802df9d40409a9dd6d
codeme254 <otwomadennis365@gmail.com>
2022-08-23 20:18:37 +0300

    task 0 correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,42 +1,38 @@
 #include "main.h"
 
 /**
- * read_textfile - Entry Point
- * @filename: file name
- * @letters: size
- * Return: 0
+ * create_file - creates a file
+ * @filename: the name of the file to be created
+ * @text_content: A NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure (file can not be created,
+ * file can not be written, write “fails”, etc…)
+ *
+ * Author: Renish
  */
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-	int file, rd, wr;
-	char *buf;
 
-	if (filename == NULL)
-		return (0);
+int create_file(const char *filename, char *text_content)
+{
+	int i = 0;
+	int fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	int write_file = write(fd, text_content, i);
 
 	if (!filename)
-		return (0);
+		return (-1);
 
-	file = open(filename, O_RDONLY);
+	if (text_content)
+	{
+		for (i = 0; text_content[i];)
+			i++;
+	}
 
-	if (file == -1)
-		return (0);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	write_file = write(fd, text_content, i);
 
-	buf = malloc(sizeof(char) * letters + 1);
-	if (buf == NULL)
-		return (0);
+	if (fd == -1 || write_file == -1)
+		return (-1);
 
-	rd = read(file, buf, letters);
-	if (rd == -1)
-		return (0);
+	close(fd);
 
-	buf[letters] = '\0';
-
-	wr = write(1, buf, rd);
-	if (wr == -1)
-		return (0);
-
-	close(file);
-	free(buf);
-	return (wr);
+	return (1);
 }
