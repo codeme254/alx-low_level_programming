ee650fa631bf25fc101ffa15f236166e0e8ca6e7
codeme254 <otwomadennis365@gmail.com>
2022-08-23 20:26:02 +0300

    task 0 correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,8 +7,6 @@
  *
  * Return: 1 on success, -1 on failure (file can not be created,
  * file can not be written, write “fails”, etc…)
- *
- * Author: Renish
  */
 
 int create_file(const char *filename, char *text_content)
